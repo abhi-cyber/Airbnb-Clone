7e862495cbe236f55ec09a3abd1b4743f8875c0a
abhi-cyber <abhirajchatrath@gmail.com>
2022-05-14 11:38:45 +0530

    List of Props rendered

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import HomeScreen from './src/screens/Home/index'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView,StyleSheet, Text, View } from 'react-native';
 import Post from "./src/components/Post";
-import feed from './assets/data/feed'
+import feed from './assets/data/feed';
+import SearchResultsScreen from './src/screens/SearchResults';
 
 const post1 = feed[0];
 const post2 = feed[1];
@@ -17,9 +18,8 @@ export default function App() {
     <StatusBar barStyle="dark-content" />
     <SafeAreaView style={styles.container}>
       {/*<HomeScreen />*/}
-      <Post post={post1} />
-      <Post post={post2} />
-      <Post post={post3} />
+      {/*<Post post={post2} />*/}
+      <SearchResultsScreen />
     </SafeAreaView>
     </>
   );
---
+++ src/screens/SearchResults/index.js
@@ -0,0 +1,18 @@
+import { View, FlatList } from 'react-native';
+import React from 'react';
+import feed from '../../../assets/data/feed';
+import Post from '../../components/Post';
+
+
+const SearchResultsScreen = (props) => {
+    return(
+        <View>
+            <FlatList
+                data={feed}
+                renderItem={({item}) => <Post post={item} />}
+            />
+        </View>
+    );
+};
+
+export default SearchResultsScreen;
