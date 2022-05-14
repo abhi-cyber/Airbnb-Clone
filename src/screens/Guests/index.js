a2799b57adbfa515cd852f59f2475a8d41dfd3f0
abhi-cyber <abhirajchatrath@gmail.com>
2022-05-14 20:37:00 +0530

    Counter Updated | Children and Infant Fields Added

--- src/screens/Guests/index.js
+++ src/screens/Guests/index.js
@@ -6,6 +6,8 @@ import { useState } from 'react';
 const GuestsScreen = () => {
 
     const [adults, setAdults] = useState(0);
+    const [childrens, setChildrens] = useState(0);
+    const [infants, setInfants] = useState(0);
 
   return (
     <View>
@@ -15,13 +17,13 @@ const GuestsScreen = () => {
             {/* Titles */}
             <View>
                 <Text style={{fontWeight: 'bold'}}>Adults</Text>
-                <Text style={{color: '#8d8d8d'}}>Age 16 or above</Text>
+                <Text style={{color: '#8d8d8d'}}>Ages 16 or above</Text>
             </View>
             {/* Buttons with value */}
             <View style={{flexDirection: 'row', alignItems: 'center'}}>
                 {/* - */}
                 <Pressable 
-                onPress={() => setAdults(adults - 1)} // Check data not written
+                onPress={() => setAdults(Math.max(0, adults - 1))}
                 style={styles.button}
                 >
                     <Text style={{fontSize: 20, color: '#474747'}}>-</Text>
@@ -30,14 +32,69 @@ const GuestsScreen = () => {
                 <Text style={{marginHorizontal: 20, fontSize:16}}>{adults}</Text>
                 {/* + */}
                 <Pressable 
-                onPress={() => setAdults(adults + 1)} // Check data not written
+                onPress={() => setAdults(adults + 1)} 
                 style={styles.button}
                 >
                     <Text style={{fontSize: 20, color: '#474747'}}>+</Text>
                 </Pressable>
             </View>
         </View>
-        {/* Row 1: Adults */}
+        {/* Row 2: Children */}
+        <View style={styles.row}>
+
+            {/* Titles */}
+            <View>
+                <Text style={{fontWeight: 'bold'}}>Children</Text>
+                <Text style={{color: '#8d8d8d'}}>Ages 2-12</Text>
+            </View>
+            {/* Buttons with value */}
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                {/* - */}
+                <Pressable 
+                onPress={() => setChildrens(Math.max(0, childrens - 1))}
+                style={styles.button}
+                >
+                    <Text style={{fontSize: 20, color: '#474747'}}>-</Text>
+                </Pressable>
+                
+                <Text style={{marginHorizontal: 20, fontSize:16}}>{childrens}</Text>
+                {/* + */}
+                <Pressable 
+                onPress={() => setChildrens(childrens + 1)} 
+                style={styles.button}
+                >
+                    <Text style={{fontSize: 20, color: '#474747'}}>+</Text>
+                </Pressable>
+            </View>
+        </View>
+        {/* Row 3: Infants */}
+        <View style={styles.row}>
+
+            {/* Titles */}
+            <View>
+                <Text style={{fontWeight: 'bold'}}>Infants</Text>
+                <Text style={{color: '#8d8d8d'}}>Under 2</Text>
+            </View>
+            {/* Buttons with value */}
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                {/* - */}
+                <Pressable 
+                onPress={() => setInfants(Math.max(0, infants - 1))} 
+                style={styles.button}
+                >
+                    <Text style={{fontSize: 20, color: '#474747'}}>-</Text>
+                </Pressable>
+                
+                <Text style={{marginHorizontal: 20, fontSize:16}}>{infants}</Text>
+                {/* + */}
+                <Pressable 
+                onPress={() => setInfants(infants + 1)} 
+                style={styles.button}
+                >
+                    <Text style={{fontSize: 20, color: '#474747'}}>+</Text>
+                </Pressable>
+            </View>
+        </View>
 
     </View>
   )
