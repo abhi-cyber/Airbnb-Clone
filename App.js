57a26077514552dcab4092778471c21975ab964a
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-01 19:51:19 +0530

    Expo updated

--- App.js
+++ App.js
@@ -3,9 +3,6 @@ import HomeScreen from './src/screens/Home/index';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-gesture-handler';
 import Router from './src/navigation/Router';
-// import { Amplify } from 'aws-amplify'
-// import awsconfig from './src/aws-exports'
-// Amplify.configure(awsconfig)
 
 export default function App() { 
   
