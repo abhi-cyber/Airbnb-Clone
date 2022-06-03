b247353d930106bb73847560cb23ac9468d81b53
abhi-cyber <abhirajchatrath@gmail.com>
2022-06-03 22:04:54 +0530

    Card Styling

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -2,7 +2,7 @@ import { View, TouchableOpacity  } from 'react-native';
 import React from 'react';
 import styles from './styles';
 import {useNavigation} from '@react-navigation/native';
-import { Card, CardTitle, CardImage } from 'react-native-material-cards';
+import { Card, CardImage } from 'react-native-material-cards';
 import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const DestinationSearchScreen = () => {
@@ -13,24 +13,31 @@ const DestinationSearchScreen = () => {
 
     const del = { uri : "https://media.istockphoto.com/vectors/new-delhi-city-line-art-vector-illustration-with-all-famous-buildings-vector-id1251170836?k=20&m=1251170836&s=612x612&w=0&h=VWVJpfOBaBwqQOF-qtEFMzCDaRA0FnDn8PvajUJq1nA=" };
     
-    const pub = { uri : "https://media.istockphoto.com/vectors/young-woman-sunbathing-on-a-beach-vector-id1137918812?k=20&m=1137918812&s=612x612&w=0&h=ldg-CVvTjl2kSGo2UZRYa-4wxH_AFbdDpu1UnNxA_Zs=" };
+    const goa = { uri : "https://media.istockphoto.com/vectors/young-woman-sunbathing-on-a-beach-vector-id1137918812?k=20&m=1137918812&s=612x612&w=0&h=ldg-CVvTjl2kSGo2UZRYa-4wxH_AFbdDpu1UnNxA_Zs=" };
   return (    
         <Pressable onPress={(data, details = null) => {
                         // 'details' is provided when fetchDetails = true
                         console.log(data, details);
                         navigation.navigate('Guests');
                     }}>
-             <View style={styles.container}>
+            <View style={styles.container}>
+                <View style={styles.cont}>
 
-                <Card style={styles.card}>
-                    <CardImage source={chd} style = {styles.image}/>
-                </Card>
-                <Card style={styles.card}>
-                    <CardImage source={del} style = {styles.image}/>
-                </Card>
-                <Card style={styles.card}>
-                    <CardImage source={pub} style = {styles.image}/>
-                </Card>
+                    <Card style={styles.card}>
+                        <CardImage source={chd} style = {styles.image}/>
+                    </Card>
+                    <Card style={styles.card}>
+                        <CardImage source={del} style = {styles.image}/>
+                    </Card>
+                </View>
+                <View style={styles.cont}>
+                    <Card style={styles.card}>
+                        <CardImage source={goa} style = {styles.image}/>
+                    </Card>
+                    <Card style={styles.card}>
+                        <CardImage source={goa} style = {styles.image}/>
+                    </Card>
+                </View>
             </View>
         </Pressable>
   )
--- src/screens/DestinationSearch/styles.js
+++ src/screens/DestinationSearch/styles.js
@@ -4,15 +4,20 @@ const styles =StyleSheet.create({
     container: {
         padding:20,
         flexDirection: "column",
-        height: '100%',
+        height: 500,
+        flexWrap: 'wrap',
+    },
+    cont: {
+        flexDirection: 'row',
+        width: '100%',
+        height: 225,
     },
     card:{
-        flex: 1,
+        width: '50%',
         borderRadius: 15,
-        position: 'relative'
     },
     image: {
-        position: 'absolute',
+        
         height: '100%',
         width: '100%'
     },
